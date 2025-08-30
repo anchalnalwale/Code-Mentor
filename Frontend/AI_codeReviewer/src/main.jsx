223f0dda658693d973e7bc87fdaee181f2b5ef0f
anchalnalwale <nalwaleanchal@gmail.com>
2025-08-30 14:50:18 +0530

    Changes in main.jsx

--- Frontend/AI_codeReviewer/src/main.jsx
+++ Frontend/AI_codeReviewer/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('API Error:', error.response?.data || error.message);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
